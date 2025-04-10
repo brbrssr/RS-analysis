c8ec759a9fe50bc326a0aa7a42aecbbd37e70fcc
san4ez1337 <urazmetovzufar524@gmail.com>
2025-04-11 00:07:35 +0500

    trying to implement the interval picker

---
+++ rsapp/src/interval_picker.py
@@ -0,0 +1,31 @@
+import flet as ft
+
+class IntervalPicker:
+    # IntervalPicker class constructor
+    def __init__(self, on_change=None):
+        super().__init__()
+        self.on_change_callback = on_change
+
+        self.amount_tf = ft.TextField(label="Amount", width=150)
+        self.unit_dd = ft.Dropdown(label="Units", width=150, 
+                                   options=[])
+
+        self.controls = ft.Row([self.amount_tf, self.unit_dd])
+    
+        self.amount_tf.on_change = self._on_any_change
+        self.unit_dd.on_change = self._on_any_change
+    
+
+    def _on_any_change(self, e):
+        if self.on_change_callback:
+            self.on_change_callback(self)
+
+
+    def get_interval(self):
+        if self.amount_tf.value and self.unit_dd.value:
+            return f"{self.amount_tf.value}:{self.unit_dd.value}"
+        return None
+    
+
+    def render(self):
+        return self.controls
--- rsapp/src/main.py
+++ rsapp/src/main.py
@@ -2,21 +2,21 @@ import flet as ft
 import date_picker
 import time_picker
 import trade_pair_input
+import interval_picker
 
 
 def main(page: ft.Page):
     result_date = ft.Text() # Text fields. Just for debug
     result_time = ft.Text()
     result_trade_pair = ft.Text()
+    result_interval = ft.Text()
     
 
     def on_date_change(date_picker):
         date = date_picker.get_date()
         result_date.value = f"Date: {date}" if date else "Date not selected"
         page.update()
-    
     datePicker = date_picker.DatePicker(on_change=on_date_change)
-
     page.add(datePicker.render(), result_date)
 
 
@@ -24,9 +24,7 @@ def main(page: ft.Page):
         time = time_picker.get_time()
         result_time.value = f"Time: {time}" if time else "Time not selected"
         page.update()
-    
     timePicker = time_picker.TimePicker(on_change=on_time_change)
-
     page.add(timePicker.render(), result_time)
 
 
@@ -34,10 +32,16 @@ def main(page: ft.Page):
         trade_pair = trade_pair_input.get_trade_pair()
         result_trade_pair.value = f"Trade Pair: {trade_pair}" if trade_pair else "Trade Pair not selected"
         page.update()
-
     tradePair = trade_pair_input.TradePairInput(on_change=on_trade_pair_change)
-
     page.add(tradePair.render(), result_trade_pair)
+
+
+    def on_interval_change(interval_picker):
+        interval = interval_picker.get_interval()
+        result_interval.value = f"Interval: {interval}" if interval else "Interval not selected"
+        page.update()
+    intervalPicker = interval_picker.IntervalPicker(on_change=on_interval_change)
+    page.add(intervalPicker.render(), result_interval)
 
 
 ft.app(main)
